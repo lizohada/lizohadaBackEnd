53a13bfb00c7f8fd469923e1cf941e07b81f4579
ohchansol <haxr369@gmail.com>
2024-03-08 14:08:55 +0900

    zip 생성 및 람다 테스트 성공

--- crawler/index.mjs
+++ crawler/index.mjs
@@ -1,6 +1,6 @@
-import { blogScraper } from "./blogScraper.js";
-import { blogDetailScraper } from "./blogDetailScraper.mjs";
-
+import { blogScraper } from "./common/blogScraper.js";
+import { blogDetailScraper } from "./common/blogDetailScraper.mjs";
+import { fs } from "fs";
 /**
  * event = {
  *  keyword : "울릉 여행",
@@ -25,11 +25,22 @@ export const handler = async (event, context) => {
     return { statusCode: 400, body: "No content provided" };
   }
   let blogs = await blogScraper(body.keyword, body.count);
+  // map은 함수 콜을 하고 그 결과를 기다리고 다음 콜한다. 100개 스크랩에 -> 37초
+  // blogs = await Promise.all(
+  //   blogs.map((blog) => {
+  //     return blogDetailScraper(blog);
+  //   })
+  // );
+  // map은 각 함수 콜을 하고 100개 스크랩에 -> 21초
   blogs = await Promise.all(
     blogs.map(async (blog) => {
       return await blogDetailScraper(blog);
     })
   );
+
+  const filePath = "./file.json";
+
+  fs.writeFileSync(filePath, JSON.stringify(blogs, null, 2), "utf-8");
   return {
     statusCode: 200,
     body: JSON.stringify(blogs),
--- crawler/local.mjs
+++ crawler/local.mjs
@@ -1,7 +1,7 @@
 import { handler } from "./index.mjs";
 const event = {
   keyword: "울릉 여행",
-  count: "30",
+  count: "100",
 }; // 테스트하고 싶은 내용으로 변경
 const context = {};
 
