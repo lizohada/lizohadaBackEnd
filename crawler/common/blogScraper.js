d1879142936a2d10b299ad98e20998c109eb25f4
ohchansol <haxr369@gmail.com>
2024-03-13 16:07:10 +0900

    lambda가 DynamoDB에 글을 저장할 수 있도록 & cloudformation으로 ec2 생성

--- crawler/common/blogScraper.js
+++ crawler/common/blogScraper.js
@@ -4,12 +4,12 @@ const axios = require("axios");
 var client_id = process.env.NAVER_CLIENT_ID;
 var client_secret = process.env.NAVER_CLIENT_SECRECT;
 
-async function blogScraper(keyword, count) {
+async function blogScraper(keyword, count, recentPostDate) {
   try {
     let result = [];
     let start = 1;
     const display = 10;
-    while (start <= count) {
+    while (result.length < count) {
       var api_url = `https://openapi.naver.com/v1/search/blog?display=${display}&start=${start}&sort=date&query=\"${encodeURIComponent(
         keyword
       )}\"`;
@@ -32,7 +32,20 @@ async function blogScraper(keyword, count) {
             postdate: item.postdate,
           };
         });
-        result = [...result, ...data];
+        const filteredData = data.filter(function (item) {
+          console.log(recentPostDate, item.postdate);
+          console.log(item.postdate > recentPostDate);
+          return item.postdate > recentPostDate;
+        });
+        result = [...result, ...filteredData];
+
+        if (result.length > count) {
+          result = result.slice(0, count + 1);
+        }
+        if (filteredData.length !== data.length) {
+          console.log("스크랩 목표 포스팅 개수 : " + result.length);
+          return result;
+        }
       }
       start += display;
     }
