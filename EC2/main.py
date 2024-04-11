75f9ff29cc8ae4d025131a6e22708a2d79fc8b99
toctoce <toctoce@naver.com>
2024-04-12 00:08:40 +0900

    get query api 작성 완료

--- EC2/main.py
+++ EC2/main.py
@@ -4,18 +4,20 @@ import boto3
 from boto3.dynamodb.conditions import Key, Attr
 from KeywordLearning import keyword_learning
 from PreferenceInference import preference_inference
+import random
 
 app = FastAPI()
 
 # Get the service resource.
 dynamodb = boto3.resource('dynamodb')
 # 테이블 접속
-table = dynamodb.Table('keyword_blog_contents') # 스크랩한 블로그 글
+keyword_blog_contents_table = dynamodb.Table('keyword_blog_contents') # 스크랩한 블로그 글
+regionAndKeywords_table = dynamodb.Table('regionAndKeywords') # 스크랩한 블로그 글
 
 @app.get("/")
 def read_root():
   # keyword가 같은 쿼리와 같은 요소를 전부 가져온다.
-  response = table.query(
+  response = keyword_blog_contents_table.query(
       KeyConditionExpression=Key('keyword').eq('광주 여행')
   )
   items = response['Items']
@@ -29,7 +31,15 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 @app.get("/keyword/query")
 def get_query():
-    return {}
+    response = regionAndKeywords_table.scan(AttributesToGet=['keyword'])
+    all_records = response['Items']
+
+    keywords_set = set()
+    while len(keywords_set) < 12:
+        random_record = random.choice(all_records)
+        keywords_set.add(random_record['keyword'])
+
+    return keywords_set
 
 model = preference_inference.load_model()
 
